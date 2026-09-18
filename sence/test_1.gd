0d62a65128a0f9eeeac1086a8af63f89647bee9b
lucaskuo119 <lucas119kuo@gmail.com>
2026-09-18 13:59:20 +0800

    更新遊戲內容

--- sence/test_1.gd
+++ sence/test_1.gd
@@ -3,12 +3,17 @@ extends Node2D
 var time_left = 0
 var running_check = false
 var coin:int = 0
-const save_path = "user://save.json"
+var total_time = 0
 
+
+
+
+const save_path = "user://save.json"
 
 func _ready() -> void:
 	lode_game()
-	$Control/Panel/coin_counter.text = "🪙" + str(coin)
+	
+	$Control/Panel/coin_counter.text = str(coin)
 	update_display()
 
 
@@ -37,19 +42,24 @@ func _process(delta: float) -> void:
 
 func _on_start_pressed() -> void:
 	running_check = true
+	total_time = time_left
 	$Control/Timer.start()
 	$Control/coin_timer.start()
 	$Control/add_time.disabled = running_check
 	$Control/mines_time.disabled = running_check
-	$an_fish.play("hook")
-
-
+	$"Control/15minadd".disabled = running_check
+	$"Control/25minadd".disabled = running_check
+	$"Control/45minadd".disabled = running_check
+	$"Control/60minadd".disabled = running_check
+	$Control/start.disabled = running_check
+	
 func _on_timer_timeout() -> void:
 	if time_left > 0:
 		time_left -= 1
+		$ProgressBar.value = (1.0 - float(time_left) / total_time) * 100
 		update_display()
 	else:
-		$Timer.stop()
+		$Control/Timer.stop()
 		running_check = false
 
 
@@ -72,7 +82,7 @@ func update_display():
 func _on_coin_timer_timeout() -> void:
 	if time_left > 0:
 		coin += 1
-		$Control/Panel/coin_counter.text = "🪙" + str(coin)
+		$Control/Panel/coin_counter.text = str(coin)
 		save_game()
 	else :
 		$Control/coin_timer.stops()
@@ -84,3 +94,21 @@ func _on_erase_save_pressed() -> void:
 	coin = 0
 	save_game()
 	update_display()
+
+
+func one_five_on_minadd_pressed() -> void:
+	time_left = 900
+	update_display()
+func two_five_on_minadd_pressed() -> void:
+	time_left = 1500
+	update_display()
+func fore_five_on_minadd_pressed() -> void:
+	time_left = 2700
+	update_display()
+func six_O_on_minadd_pressed() -> void:
+	time_left = 3600
+	update_display()
+
+
+func _on_return_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://sence/main_screen.tscn")
